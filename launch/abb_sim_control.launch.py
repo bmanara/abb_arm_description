5632097746224c162bf209534437732facc53dbd
bmanara <bmacraze@gmail.com>
2025-06-25 14:05:20 +0800

    Add gazebo support

---
+++ launch/abb_sim_control.launch.py
@@ -0,0 +1,71 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    abb_arm_description_dir = get_package_share_directory('abb_arm_description')
+    abb_arm_urdf = os.path.join(abb_arm_description_dir, 'urdf', 'robot.urdf')
+
+    robot_description = Command(['xacro ', abb_arm_urdf])
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{
+            'robot_description': robot_description,
+            'use_sim_time': True    
+        }],
+        emulate_tty=True,
+    )
+
+    rviz2_config = os.path.join(abb_arm_description_dir, 'rviz', 'abb_arm.rviz')
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', rviz2_config],
+    )
+
+    world = os.path.join(abb_arm_description_dir, 'worlds', 'empty.world')
+    gazebo_params_path = os.path.join(abb_arm_description_dir, 'config', 'gazebo_params.yaml')
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('ros_gz_sim'),
+                'launch',
+                'gz_sim.launch.py'
+            )
+        ]),
+        launch_arguments={'gz_args': ['--render-engine ', 'ogre2 ', '-r ', world], 
+                          'extra_gazebo_args': ' --ros-args --params-file ' + gazebo_params_path}.items()
+    )
+
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-world', 'empty_world', '-topic', '/robot_description', '-name', 'abb_arm'],
+        output='screen'
+    )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        rviz2_node,
+        gazebo,
+        spawn_entity,
+        joint_state_publisher_node
+    ])
